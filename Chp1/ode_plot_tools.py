497edd5a0424e5af2d4e5b48d9321cfc2a7d8548
Troy Butler <butler.troy.d@gmail.com>
2022-07-20 15:05:57 -0600

    Making order t, x consistent and other minor changes

--- Chp1/ode_plot_tools.py
+++ Chp1/ode_plot_tools.py
@@ -14,7 +14,7 @@ def slope_field(t, x, diffeq,
                 **args):
     """Plots slope field given an ode
     
-    Given an ode of the form: dx/dt = f(x, t), plot a slope field (aka direction field) for given t and x arrays. 
+    Given an ode of the form: dx/dt = f(t, x), plot a slope field (aka direction field) for given t and x arrays. 
     Extra arguments are passed to matplotlib.pyplot.quiver
 
     Parameters
@@ -45,7 +45,7 @@ def slope_field(t, x, diffeq,
     if scale is not None:
         scale = 1/scale
     T, X = np.meshgrid(t, x)  # create rectangular grid with points
-    slopes = diffeq(X, T)
+    slopes = diffeq(T, X)
     dt = np.ones(slopes.shape)  # dt = an array of 1's with same dimension as diffeq
     dxu = slopes / np.sqrt(dt**2 + slopes**2)  # normalize dx
     dtu = dt / np.sqrt(dt**2 + slopes**2)  # normalize dt
@@ -60,10 +60,10 @@ def slope_field(t, x, diffeq,
     return ax
 
 
-def plot_sol(t, x, diffeq, x0, ax, npts=100, clear=False):
+def plot_sol(t, x, diffeq, x0, ax = None, npts=100, clear=False):
     """Plot Slope field and estimated solution given initial conidtion
     
-    Given an ode of the form: dx/dt = f(x, t), plot a slope field (aka direction field) for given t and x arrays. 
+    Given an ode of the form: dx/dt = f(t, x), plot a slope field (aka direction field) for given t and x arrays. 
     Given an initial condition x0, plot a solution line estimated with `odeint`.
     Extra arguments are passed to matplotlib.pyplot.quiver
 
@@ -80,7 +80,7 @@ def plot_sol(t, x, diffeq, x0, ax, npts=100, clear=False):
         The function f(t,x) = dx/dt
 
     x0 : float
-        Initial condition
+        Initial condition at t[0]
         
     ax : pyplot plotting axes
     
@@ -94,29 +94,32 @@ def plot_sol(t, x, diffeq, x0, ax, npts=100, clear=False):
         plotting axis with formatted quiver plot
     """
     
+    if (ax is None):
+        fig, ax = plt.subplots()
     if clear:
         ax.cla()
 
     slope_field(t, x, diffeq, color='grey', ax = ax)
     
-    # xlim = ax.get_xlim()
-    # ylim = ax.get_ylim()
-    
     # Plot solution
     tt = np.linspace(t.min(),t.max(),100)
-    sol = odeint(diffeq, x0, tt)
-    ax.plot(tt, sol)
+    sol = odeint(diffeq, x0, tt, tfirst=True)
+    
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
     
-    # ax.set_ylim(ylim)
-    # ax.set_xlim(xlim)
+    ax.plot(tt, sol)
+    
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
     
     plt.show()
+    return ax
     
     
 # Plot Slope field and solution given analytical solution
 def plot_dt(t, x, diffeq, t0, x0, dt, nsteps, npts=100, clear=False):
+    
     fig, ax = plt.subplots(1,1)
     if clear:
         ax.cla()
@@ -141,13 +144,13 @@ def plot_dt(t, x, diffeq, t0, x0, dt, nsteps, npts=100, clear=False):
     
 
 # Euler Methods
-def forward_euler(f, Delta_t, n, start_t, y_0):
-    # Assuming f is passed as a function of (y, t)
-    v = np.zeros(n+1)  # set each y_i by 0 at first
-    v[0] = y_0  # set first value to y_0
+def forward_euler(f, dt, n, start_t, x_0):
+    # Assuming f is passed as a function of (t, x)
+    v = np.zeros(n+1)  # set each x_i by 0 at first
+    v[0] = x_0  # set first value to x_0
     
     for i in range(0, n):
-        v[i+1] = v[i] + Delta_t * f(v[i], start_t + i*Delta_t)  # Euler's method formula
+        v[i+1] = v[i] + dt * f(start_t + i*dt, v[i])  # Euler's method formula
     return v
 
 
@@ -161,7 +164,7 @@ def plot_euler(t, x, diffeq, x0, dt, ax, clear=False):
 
     # Plot exact
     tt = np.linspace(t.min(),t.max(),100)
-    sol = odeint(diffeq, x0, tt)
+    sol = odeint(diffeq, x0, tt, tfirst=True)
     ax.plot(tt, sol)
     
     # Store limits to keep approx from changing
